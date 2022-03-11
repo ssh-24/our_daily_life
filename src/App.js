5ea23167274fa572a84f792911552b7ce7f0ff36
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-03-12 01:42:18 +0900

    220312 01:42 component 추가 & 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, { Component } from 'react';
+import Header from './layout/Header';
+import Lists from './components/Lists';
 import './assets/css/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello!!
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      header:{title:'Our Daily Life', sub: '우리의 일상'},
+      list:[
+        {id:1, title:'SSH', desc:'Some description'},
+        {id:2, title:'DSH', desc:'Some description'},
+        {id:3, title:'LSS', desc:'Some description'},
+        {id:4, title:'HJH', desc:'Some description'},
+        {id:5, title:'YSL', desc:'Some description'},
+        {id:6, title:'LJM', desc:'Some description'},
+        {id:7, title:'ACS', desc:'Some description'},
+        {id:8, title:'SSJ', desc:'Some description'},
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header
+          title={this.state.header.title}
+          sub={this.state.header.sub}>
+        </Header>
+        <Lists data={this.state.list}></Lists>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/layout/header.js
+++ src/layout/header.js
@@ -1,1 +1,16 @@
 // 헤더
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1 className="title">{this.props.title}</h1>
+                <p className="sub-title">{this.props.sub}</p>
+            </header>
+        );
+    }
+}
+
+export default Header;
+
