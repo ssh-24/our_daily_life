859e61fba4827b3d477c24fa0ca52de2cb40c937
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-03-14 21:21:55 +0900

    220314 21:21

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome',
       header:{title:'Our Daily Life', sub: '우리의 일상'},
+      welcome: {title:'Welcome', desc:'Let\'s start!'},
       list:[
         {id:1, title:'SSH', desc:'Some description'},
         {id:2, title:'DSH', desc:'Some description'},
@@ -17,15 +19,25 @@ class App extends Component {
         {id:6, title:'LJM', desc:'Some description'},
         {id:7, title:'ACS', desc:'Some description'},
         {id:8, title:'SSJ', desc:'Some description'},
-      ]
+      ],
     }
   }
   render() {
+    let _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read') {
+      _title = this.state.header.title;
+      _desc = this.state.header.sub;
+    }
+
     return (
       <div className="App">
         <Header
-          title={this.state.header.title}
-          sub={this.state.header.sub}>
+          title={_title}
+          sub={_desc}>
         </Header>
         <Lists data={this.state.list}></Lists>
       </div>
