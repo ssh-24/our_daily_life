1bc695153ae6a36a5464933a0a5fd183ca476733
ssh-24 <sonsh5364@gmail.com>
2023-04-10 16:07:22 +0900

    230410 16:07 로직 대충 수정

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 import React from "react";
 import Logo from "../assets/images/logo.png";
 import { useAuthContext } from '../hooks/useAuthContext';
@@ -9,34 +10,26 @@ function Nav(props) {
     const {logout} = useLogout();
     const {user} = useAuthContext();
 
-    const renewClicked = (userInfo) => {
-        if (userInfo.email === '') return;
-        alert('피드 갱신');
+    const renewClicked = () => {
         // 갱신 요청 logic
-
+        alert('피드 갱신')
     }
 
     const searchAjaxRequest = (search_text) => {
-        // Ajax 통신 필요
         console.log(search_text);
+        // Ajax 통신 필요
     }
 
-    const uploadClicked = (userInfo) => {
-        if (userInfo.email === '') return;
-        alert('새 게시물 만들기');
-
+    const uploadClicked = () => {
+        alert('게시물 업로드')
     }
 
-    const logClicked = (userInfo) => {
-        if (userInfo.email === '') return;
-        alert(`${userInfo.nickname}의 활동 기록 조회`);
-
+    const logClicked = () => {
+        alert(`활동 로그 조회`)
     }
 
-    const myprofileClicked = (userInfo) => {
-        if (userInfo.email === '') return;
-        alert(`${userInfo.nickname}의 프로필`);
-
+    const myprofileClicked = () => {
+        alert(`프로필 조회`)
     }
 
     return (
@@ -45,7 +38,7 @@ function Nav(props) {
                 <div className="refresh">
                     <a href="/renew" tabIndex="0" onClick={(e)=>{
                         e.preventDefault();
-                        renewClicked(props.userInfo);
+                        renewClicked();
                     }}>
                         <div className="logo_div">
                             <img alt="Our Daily Life" className="logo_img" src={Logo}/>
@@ -54,71 +47,44 @@ function Nav(props) {
                 </div>
 
                 <div className="user-search">
-                    <div className="btn-item">
-                        <svg aria-label="검색" color="#262626"                            
-                            fill="#262626" role="img" viewBox="0 0 20 20">
-                            <path d="M18.109,17.776l-3.082-3.081c-0.059-0.059-0.135-0.077-0.211-0.087c1.373-1.38,2.221-3.28,2.221-5.379c0-4.212-3.414-7.626-7.625-7.626c-4.212,0-7.626,3.414-7.626,7.626s3.414,7.627,7.626,7.627c1.918,0,3.665-0.713,5.004-1.882c0.006,0.085,0.033,0.17,0.098,0.234l3.082,3.081c0.143,0.142,0.371,0.142,0.514,0C18.25,18.148,18.25,17.918,18.109,17.776zM9.412,16.13c-3.811,0-6.9-3.089-6.9-6.9c0-3.81,3.089-6.899,6.9-6.899c3.811,0,6.901,3.09,6.901,6.899C16.312,13.041,13.223,16.13,9.412,16.13z"></path>
-                        </svg>
-                    </div>
-                    <input aria-label="입력 검색" autoCapitalize="none" className="search_input" placeholder="검색" type="text"
-                    onChange={(e)=> {
-                        // Ajax 통신으로 관련 리스트 호출해오는 로직 구현
-                        searchAjaxRequest(e.target.value);
+                    <SearchBtn className="search-btn"/>
+                    <input aria-label="검색" autoCapitalize="none" className="search_input" placeholder="검색" type="text"
+                        onChange={(e)=> {
+                            // Ajax 통신으로 관련 리스트 호출해오는 로직 구현
+                            searchAjaxRequest(e.target.value);
                     }}/>
                 </div>
             
                 <div className="btn-list">
                     <div className="btn-item">
-                        <a className="home-btn" href="/renew" tabIndex="0" onClick={(e)=>{
+                        <HomeBtn onClick={(e)=>{
                             e.preventDefault();
-                            renewClicked(props.userInfo);
-                        }}>
-                            <svg aria-label="홈" color="#262626"
-                            fill="#262626" role="img" viewBox="0 0 20 20">
-                            <path d="M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z"></path>
-                            </svg>
-                        </a>
+                            renewClicked()}}/>
+                    </div>
+                    <div className="btn-item">
+                        <AddBtn onClick={(e)=>{
+                            e.preventDefault();
+                            uploadClicked();
+                        }}/>
                     </div>
 
                     <div className="btn-item">
-                        <a className="upload-btn" href="/upload" tabIndex="0" onClick={(e)=>{
+                        <LikeBtn onClick={(e)=>{
                             e.preventDefault();
-                            uploadClicked(props.userInfo);
-                        }}>
-                            <svg aria-label="새로운 게시물" color="#262626"
-                                fill="#262626" role="img" viewBox="0 0 20 20">
-                            <path d="M14.613,10c0,0.23-0.188,0.419-0.419,0.419H10.42v3.774c0,0.23-0.189,0.42-0.42,0.42s-0.419-0.189-0.419-0.42v-3.774H5.806c-0.23,0-0.419-0.189-0.419-0.419s0.189-0.419,0.419-0.419h3.775V5.806c0-0.23,0.189-0.419,0.419-0.419s0.42,0.189,0.42,0.419v3.775h3.774C14.425,9.581,14.613,9.77,14.613,10 M17.969,10c0,4.401-3.567,7.969-7.969,7.969c-4.402,0-7.969-3.567-7.969-7.969c0-4.402,3.567-7.969,7.969-7.969C14.401,2.031,17.969,5.598,17.969,10 M17.13,10c0-3.932-3.198-7.13-7.13-7.13S2.87,6.068,2.87,10c0,3.933,3.198,7.13,7.13,7.13S17.13,13.933,17.13,10"></path>
-                            </svg>
-                        </a>
+                            logClicked();
+                        }}/>
                     </div>
 
                     <div className="btn-item">
-                        <a className="log-btn" href="/log" tabIndex="0" onClick={(e)=>{
+                        <ProfileBtn onClick={(e)=>{
                             e.preventDefault();
-                            logClicked(props.userInfo);
-                        }}>                                
-                            <svg aria-label="활동 기록" color="#262626" 
-                            fill="#262626" role="img" viewBox="0 0 20 20">
-                            <path d="M13.22,2.984c-1.125,0-2.504,0.377-3.53,1.182C8.756,3.441,7.502,2.984,6.28,2.984c-2.6,0-4.714,2.116-4.714,4.716c0,0.32,0.032,0.644,0.098,0.96c0.799,4.202,6.781,7.792,7.46,8.188c0.193,0.111,0.41,0.168,0.627,0.168c0.187,0,0.376-0.041,0.55-0.127c0.011-0.006,1.349-0.689,2.91-1.865c0.021-0.016,0.043-0.031,0.061-0.043c0.021-0.016,0.045-0.033,0.064-0.053c3.012-2.309,4.6-4.805,4.6-7.229C17.935,5.1,15.819,2.984,13.22,2.984z M12.544,13.966c-0.004,0.004-0.018,0.014-0.021,0.018s-0.018,0.012-0.023,0.016c-1.423,1.076-2.674,1.734-2.749,1.771c0,0-6.146-3.576-6.866-7.363C2.837,8.178,2.811,7.942,2.811,7.7c0-1.917,1.554-3.47,3.469-3.47c1.302,0,2.836,0.736,3.431,1.794c0.577-1.121,2.161-1.794,3.509-1.794c1.914,0,3.469,1.553,3.469,3.47C16.688,10.249,14.474,12.495,12.544,13.966z"></path>
-                            </svg>
-                        </a>
+                            myprofileClicked();
+                        }}/>
                     </div>
 
                     <div className="btn-item">
-                        <a className="myprofile-btn" href="/myprofile" tabIndex="0" onClick={(e)=>{
-                            e.preventDefault();
-                            myprofileClicked(props.userInfo);
-                        }}>
-                            <svg aria-label="내 프로필" color="#262626"
-                            fill="#262626" role="img" viewBox="0 0 20 20">
-                            <path d="M10,10.9c2.373,0,4.303-1.932,4.303-4.306c0-2.372-1.93-4.302-4.303-4.302S5.696,4.223,5.696,6.594C5.696,8.969,7.627,10.9,10,10.9z M10,3.331c1.801,0,3.266,1.463,3.266,3.263c0,1.802-1.465,3.267-3.266,3.267c-1.8,0-3.265-1.465-3.265-3.267C6.735,4.794,8.2,3.331,10,3.331z"></path>
-                            <path d="M10,12.503c-4.418,0-7.878,2.058-7.878,4.685c0,0.288,0.231,0.52,0.52,0.52c0.287,0,0.519-0.231,0.519-0.52c0-1.976,3.132-3.646,6.84-3.646c3.707,0,6.838,1.671,6.838,3.646c0,0.288,0.234,0.52,0.521,0.52s0.52-0.231,0.52-0.52C17.879,14.561,14.418,12.503,10,12.503z"></path>
-                            </svg>
-                        </a>
+                       <LogoutBtn onClick={logout}/>
                     </div>
-
-                    <button type='button' onClick={logout}>로그아웃</button>
-
                 </div>
             </div>
         </nav>
@@ -126,3 +92,145 @@ function Nav(props) {
 }
 
 export default Nav;
+
+
+const SearchBtn = (props) => (
+    <svg
+      fill="#000000"
+      // width="800px"
+      // height="800px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" />
+    </svg>
+);
+
+const HomeBtn = (props) => (
+    <svg
+      // width="800px"
+      // height="800px"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7.5 0.5L7.8254 0.120372C7.63815 -0.0401239 7.36185 -0.0401239 7.1746 0.120372L7.5 0.5ZM0.5 6.5L0.174604 6.12037L0 6.27003V6.5H0.5ZM5.5 14.5V15C5.77614 15 6 14.7761 6 14.5H5.5ZM9.5 14.5H9C9 14.7761 9.22386 15 9.5 15V14.5ZM14.5 6.5H15V6.27003L14.8254 6.12037L14.5 6.5ZM1.5 15H5.5V14H1.5V15ZM14.8254 6.12037L7.8254 0.120372L7.1746 0.879628L14.1746 6.87963L14.8254 6.12037ZM7.1746 0.120372L0.174604 6.12037L0.825396 6.87963L7.8254 0.879628L7.1746 0.120372ZM6 14.5V11.5H5V14.5H6ZM9 11.5V14.5H10V11.5H9ZM9.5 15H13.5V14H9.5V15ZM15 13.5V6.5H14V13.5H15ZM0 6.5V13.5H1V6.5H0ZM7.5 10C8.32843 10 9 10.6716 9 11.5H10C10 10.1193 8.88071 9 7.5 9V10ZM7.5 9C6.11929 9 5 10.1193 5 11.5H6C6 10.6716 6.67157 10 7.5 10V9ZM13.5 15C14.3284 15 15 14.3284 15 13.5H14C14 13.7761 13.7761 14 13.5 14V15ZM1.5 14C1.22386 14 1 13.7761 1 13.5H0C0 14.3284 0.671573 15 1.5 15V14Z"
+        fill="#000000"
+      />
+    </svg>
+);
+
+const AddBtn = (props) => (
+    <svg
+    id="Capa_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 489.8 489.8"
+    style={{
+      enableBackground: "new 0 0 489.8 489.8",
+    }}
+    xmlSpace="preserve"
+    {...props}
+  >
+    <g>
+      <g>
+        <path d="M438.2,0H51.6C23.1,0,0,23.2,0,51.6v386.6c0,28.5,23.2,51.6,51.6,51.6h386.6c28.5,0,51.6-23.2,51.6-51.6V51.6 C489.8,23.2,466.6,0,438.2,0z M465.3,438.2c0,14.9-12.2,27.1-27.1,27.1H51.6c-14.9,0-27.1-12.2-27.1-27.1V51.6 c0-14.9,12.2-27.1,27.1-27.1h386.6c14.9,0,27.1,12.2,27.1,27.1V438.2z" />
+        <path d="M337.4,232.7h-80.3v-80.3c0-6.8-5.5-12.3-12.3-12.3s-12.3,5.5-12.3,12.3v80.3h-80.3c-6.8,0-12.3,5.5-12.3,12.2 c0,6.8,5.5,12.3,12.3,12.3h80.3v80.3c0,6.8,5.5,12.3,12.3,12.3s12.3-5.5,12.3-12.3v-80.3h80.3c6.8,0,12.3-5.5,12.3-12.3 C349.7,238.1,344.2,232.7,337.4,232.7z" />
+      </g>
+    </g>
+  </svg>
+);
+
+const LikeBtn = (props) => (
+    <svg
+    fill="#000000"
+    // width="800px"
+    // height="800px"
+    viewBox="0 0 36 36"
+    preserveAspectRatio="xMidYMid meet"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <title>{"heart-line"}</title>
+    <path
+      d="M18,32.43a1,1,0,0,1-.61-.21C11.83,27.9,8,24.18,5.32,20.51,1.9,15.82,1.12,11.49,3,7.64c1.34-2.75,5.19-5,9.69-3.69A9.87,9.87,0,0,1,18,7.72a9.87,9.87,0,0,1,5.31-3.77c4.49-1.29,8.35.94,9.69,3.69,1.88,3.85,1.1,8.18-2.32,12.87C28,24.18,24.17,27.9,18.61,32.22A1,1,0,0,1,18,32.43ZM10.13,5.58A5.9,5.9,0,0,0,4.8,8.51c-1.55,3.18-.85,6.72,2.14,10.81A57.13,57.13,0,0,0,18,30.16,57.13,57.13,0,0,0,29.06,19.33c3-4.1,3.69-7.64,2.14-10.81-1-2-4-3.59-7.34-2.65a8,8,0,0,0-4.94,4.2,1,1,0,0,1-1.85,0,7.93,7.93,0,0,0-4.94-4.2A7.31,7.31,0,0,0,10.13,5.58Z"
+      className="clr-i-outline clr-i-outline-path-1"
+    />
+    <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
+  </svg>
+);
+
+const ProfileBtn = (props) => (
+    <svg
+    // width="800px"
+    // height="800px"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle
+      cx={12}
+      cy={7}
+      r={4}
+      stroke="#000000"
+      strokeWidth={1.4}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4 21V17C4 15.8954 4.89543 15 6 15H18C19.1046 15 20 15.8954 20 17V21"
+      stroke="#000000"
+      strokeWidth={1.4}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const LogoutBtn = (props) => (
+    <svg
+    fill="#000000"
+    // width="800px"
+    // height="800px"
+    viewBox="0 0 36 36"
+    preserveAspectRatio="xMidYMid meet"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <title>{"logout-line"}</title>
+    <path
+      d="M7,6H23v9.8h2V6a2,2,0,0,0-2-2H7A2,2,0,0,0,5,6V30a2,2,0,0,0,2,2H23a2,2,0,0,0,2-2H7Z"
+      className="clr-i-outline clr-i-outline-path-1"
+    />
+    <path
+      d="M28.16,17.28a1,1,0,0,0-1.41,1.41L30.13,22H15.63a1,1,0,0,0-1,1,1,1,0,0,0,1,1h14.5l-3.38,3.46a1,1,0,1,0,1.41,1.41L34,23.07Z"
+      className="clr-i-outline clr-i-outline-path-2"
+    />
+    <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
+  </svg>
+);
+
+
+const LikeBtnColor = (props) => (
+    <svg
+      // width="800px"
+      // height="800px"
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 48 48"
+      {...props}
+    >
+      <path
+        fill="#F44336"
+        d="M34,9c-4.2,0-7.9,2.1-10,5.4C21.9,11.1,18.2,9,14,9C7.4,9,2,14.4,2,21c0,11.9,22,24,22,24s22-12,22-24 C46,14.4,40.6,9,34,9z"
+      />
+    </svg>
+);
