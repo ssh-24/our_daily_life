f7a01efd411e98bf8b47fe1eb4e699b5ac727035
ssh-24 <sonsh5364@gmail.com>
2023-04-11 11:25:18 +0900

    230411 11:25 loading components 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import { lazy, Suspense, useEffect, useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useAuthContext } from "./hooks/useAuthContext";
 import "./assets/css/styles.css"; // 최종 스타일로 지정
+import { useAuthContext } from "./hooks/useAuthContext";
+import Loading from './layout/Loading';
 const Start = lazy(()=> import('./components/Start'))
 const Feeds = lazy(()=> import('./components/Feeds'))
 const Input = lazy(()=> import('./components/Input'))
@@ -14,26 +15,25 @@ function App() {
 
   return(
     <>
-    {
-      isAuthReady?
-      (
-        <Suspense fallback={<div>loading...</div>}>
-        {/* Suspense로 감싸기(로딩중 보여줄 때) */}
-          <Routes>
-            <Route path="/" 
-                    element={user
-                            ?<Feeds/>
-                            :<Start/>}
-            ></Route>
-            <Route path="/input" 
-                    element={<Input/>}
-            ></Route>
-          </Routes>
-        </Suspense>
-      )
-      : null
-    }
-      
+      {
+        isAuthReady?
+        (
+          <Suspense fallback={<Loading/>}>
+          {/* Suspense로 감싸기(로딩중 보여줄 때) */}
+            <Routes>
+              <Route path="/" 
+                      element={user
+                              ?<Feeds/>
+                              :<Start/>}
+              ></Route>
+              <Route path="/input" 
+                      element={<Input/>}
+              ></Route>
+            </Routes>
+          </Suspense>
+        )
+        : null
+      }
     </>
   );
 
---
+++ src/layout/Loading.js
@@ -0,0 +1,32 @@
+/*eslint-disable */
+import React from 'react';
+import styled from 'styled-components';
+import Spinner from '../assets/images/spinner.gif';
+
+// styled-components
+const Background = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.div`
+    margin-top: 12px;
+`;
+
+export default() => {
+    return (
+      <Background>
+        <img src={Spinner} alt="로딩중" width="4%" />
+        <LoadingText>Loading...</LoadingText>
+      </Background>
+    );
+};
