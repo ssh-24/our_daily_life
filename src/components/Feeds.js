3eaaaf32b11013afe096e6f7accfa994b46a10bf
ssh-24 <sonsh5364@gmail.com>
2023-04-27 17:44:33 +0900

    230427 17:44 주석정리

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -5,26 +5,31 @@ import { useSelector } from "react-redux";
 import { useCollection } from '../hooks/useCollection';
 
 function Feeds() {
-    const postList = useSelector((state) => state.feeds); // feeds 게시물 리스트
-    console.log("로컬에 저장된 피드",postList);
+    const postList = useSelector((state) => state.feeds); // 로컬 샘플 데이터
+    const {documents,error} = useCollection("FeedData"); // 서버 리얼 데이터
 
-    const {documents,error} = useCollection("FeedData");
-    console.log("Firebase 저장된 피드",documents);
+    useEffect(()=>{
+        console.log('Mock Data',postList)
+    }, [])
 
- 
+    useEffect(()=>{
+        console.log('Server Data',documents)
+    }, [documents])
+
     return (
         <>
             <div className="all-feeds">
+                {/* 리얼 데이터 출력 */}
                 {
                     documents ? 
                     documents.map((a,i) => {
                         let post = {...a}
-                        // console.log(post)
                         return (<Post key={i+postList.length} post={post}/>) 
                     })
                     : null
-                } 
+                }
 
+                {/* 샘플 데이터 출력 */}
                 {
                     postList.map((a,i) => {
                         return (<Post key={i+1} post={a} />)
