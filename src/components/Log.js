f0199c9d1acf1e04c649991b42ff482de8c4dd4c
Hwan <65848753+ssh-24@users.noreply.github.com>
2023-05-23 00:52:43 +0900

    230523 00:52 로그 페이지 1차 완료

--- src/components/Log.js
+++ src/components/Log.js
@@ -3,11 +3,20 @@ import { useState, useEffect } from 'react';
 import Post from "./Post";
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useCollectionDtl } from '../hooks/useCollectionDtl';
+import { useNavigate } from "react-router-dom";
+
 
 function Log(props) {
     const {user} = useAuthContext()
+    let navigate = useNavigate() // 페이지 이동
     const {documents,error} = useCollectionDtl("FeedData", ["peopleWhoLike","array-contains",user.uid] );
     const [docReady, setDocReady] = useState(false)
+
+    // 메인페이지로 이동 + 상단으로 스크롤 이동
+    const goMain = () => {
+        navigate('/')
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 
     useEffect(()=>{
         // documents 여부 state 변경
@@ -20,13 +29,13 @@ function Log(props) {
     useEffect(()=>{
         // documents 여부 state 변경
         setDocReady(true)
-        // documents 정렬해야할듯?
+        // 날짜, 최근 좋아요 등 기준 잡고 정렬하면 좋을 듯
         console.log("좋아하는 글 :",documents)
     },[documents])
 
     return (
         <>
-            <div className="all-feeds">
+            <div className="liked-feeds">
                 {
                     docReady && documents != null && documents.length !== 0
                     ? documents.map((a,i) => {
@@ -34,7 +43,10 @@ function Log(props) {
                         return (<Post key={i} post={post}/>) 
                     })
                     : <div className="no-like">
-                        <p>좋아하는 사진이 없어요!</p>
+                        <p>좋아하는 게시물이 없어요!</p>
+                        <button className="lookaround-btn" onClick={goMain}>
+                            피드 둘러보기
+                        </button>
                       </div>
                 }
             </div>
