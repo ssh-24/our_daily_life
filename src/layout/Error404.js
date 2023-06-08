d854e659f10a64ae6737723884c7a2db492b5afd
ssh-24 <sonsh5364@gmail.com>
2023-06-08 17:00:39 +0900

    230608 17:00 돌아가기 기능 추가

--- src/layout/Error404.js
+++ src/layout/Error404.js
@@ -1,11 +1,25 @@
 /*eslint-disable */
+import { useNavigate } from "react-router-dom";
 import ErrorImg from '../assets/images/error-404.png';
 
 export default() => {
+    let navigate = useNavigate() // 페이지 이동
+
+    // 메인페이지로 이동 + 상단으로 스크롤 이동
+    const goMain = () => {
+        navigate('/')
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
     return (
         <div className="error404">
             <img src={ErrorImg} alt="404에러" width="20%"/>
             <h2>404 not found</h2>
+            <div className="profile-go-main-btn-area">
+                <button className="go-main-btn" onClick={goMain}>
+                    돌아가기
+                </button>
+            </div>
         </div>
     );
 };
