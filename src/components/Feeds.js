43e3e99af333393bdbb873d920676c2afab178ba
ssh-24 <sonsh5364@gmail.com>
2023-06-08 17:16:57 +0900

    230608 17:16 테스트 데이터 삭제, 로직에서 제거

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -1,41 +1,28 @@
 /*eslint-disable */
 import React, { useEffect, useState } from "react";
 import Post from "./Post";
-import { useSelector } from "react-redux";
 import { useCollection } from '../hooks/useCollection';
 
 function Feeds() {
-    const postList = useSelector((state) => state.feeds); // 로컬 샘플 데이터
-    const {documents,error} = useCollection("FeedData"); // 서버 리얼 데이터
+    const {documents,error} = useCollection("FeedData"); // Feeds 전체 데이터
     let [fade, setFade] = useState('') // Animation Style State
 
     useEffect(()=>{
-        console.log('Mock Data',postList)
-    }, [])
-
-    useEffect(()=>{
-        console.log('Server Data',documents)
+        console.log('Feeds Data',documents)
         setFade(documents!= null && documents.length > 0? 'transition-end': '') // 애니메이션 효과
     }, [documents])
 
     return (
         <>
             <div className={`all-feeds transition-start ${fade}`}>
-                {/* 리얼 데이터 출력 */}
+                {/* 서버 데이터 출력 */}
                 {
                     documents ? 
                     documents.map((a,i) => {
                         let post = {...a}
-                        return (<Post key={i+postList.length} post={post}/>) 
+                        return (<Post key={a.id} post={post}/>) 
                     })
                     : null
-                }
-
-                {/* 샘플 데이터 출력 */}
-                {
-                    postList.map((a,i) => {
-                        return (<Post key={i+1} post={a} />)
-                    })
                 }
             </div>
         </>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import isSigned from './store/signedSlice'
 import inputState from './store/inputSlice'
-import feeds from './store/feedsSlice'
 import userList from './store/searchSlice'
 import loginUserInfo from './store/loginUserSlice'
 import replyState from './store/replySlice'
@@ -11,7 +10,6 @@ export default configureStore({
    reducer: {
        isSigned : isSigned.reducer,
        inputState : inputState.reducer,
-       feeds : feeds.reducer,
        userList : userList.reducer,
        loginUserInfo : loginUserInfo.reducer,
        replyState : replyState.reducer,
