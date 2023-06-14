bd93aa79821b7580a969c59e409ff3a8765766e9
ssh-24 <sonsh5364@gmail.com>
2023-06-14 15:03:57 +0900

    230614 15:03 리팩토링, 오류 수정

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -180,48 +180,32 @@ function Nav(props) {
               <div className="btn-list">
                   <div className="btn-item">
                       { props.status === 'home' ?
-                        <HomeBtnOn onClick={(e)=>{
-                          e.preventDefault()
-                          goMain()}}/>
+                        <HomeBtnOn onClick={goMain}/>
                        : 
-                        <HomeBtn onClick={(e)=>{
-                          e.preventDefault()
-                          goMain()}}/>
+                        <HomeBtn onClick={goMain}/>
                       }
                   </div>
                   <div className="btn-item">
                       { visible ?
-                        <AddBtnOn onClick={(e)=>{
-                          e.preventDefault()
-                          uploadClicked()}}/>
+                        <AddBtnOn onClick={uploadClicked}/>
                       : 
-                        <AddBtn onClick={(e)=>{
-                          e.preventDefault()
-                          uploadClicked()}}/>
+                        <AddBtn onClick={uploadClicked}/>
                       }
                   </div>
 
                   <div className="btn-item">
                       { props.status === 'log' ?
-                        <LikeBtnOn onClick={(e)=>{
-                          e.preventDefault()
-                          logClicked()}}/>
+                        <LikeBtnOn onClick={logClicked}/>
                       :
-                        <LikeBtn onClick={(e)=>{
-                          e.preventDefault()
-                          logClicked()}}/>
+                        <LikeBtn onClick={logClicked}/>
                       }
                   </div>
 
                   <div className="btn-item">
                       { props.status === 'profile' ?
-                        <ProfileBtnOn onClick={(e)=>{
-                          e.preventDefault()
-                          profileClicked()}}/>
+                        <ProfileBtnOn onClick={profileClicked}/>
                       :
-                        <ProfileBtn onClick={(e)=>{
-                          e.preventDefault()
-                          profileClicked()}}/>
+                        <ProfileBtn onClick={profileClicked}/>
                       }
                   </div>
 
@@ -245,44 +229,45 @@ export default Nav;
 
 
 const SearchBtn = (props) => (
-    <svg
-      fill="#000000"
-      // width="800px"
-      // height="800px"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" />
-    </svg>
+  <svg
+    fill="#000000"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" />
+  </svg>
 );
 
 const HomeBtn = (props) => (
-    <svg
-      // width="800px"
-      // height="800px"
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <title>{"home"}</title>
-      <path
-        d="M7.5 0.5L7.8254 0.120372C7.63815 -0.0401239 7.36185 -0.0401239 7.1746 0.120372L7.5 0.5ZM0.5 6.5L0.174604 6.12037L0 6.27003V6.5H0.5ZM5.5 14.5V15C5.77614 15 6 14.7761 6 14.5H5.5ZM9.5 14.5H9C9 14.7761 9.22386 15 9.5 15V14.5ZM14.5 6.5H15V6.27003L14.8254 6.12037L14.5 6.5ZM1.5 15H5.5V14H1.5V15ZM14.8254 6.12037L7.8254 0.120372L7.1746 0.879628L14.1746 6.87963L14.8254 6.12037ZM7.1746 0.120372L0.174604 6.12037L0.825396 6.87963L7.8254 0.879628L7.1746 0.120372ZM6 14.5V11.5H5V14.5H6ZM9 11.5V14.5H10V11.5H9ZM9.5 15H13.5V14H9.5V15ZM15 13.5V6.5H14V13.5H15ZM0 6.5V13.5H1V6.5H0ZM7.5 10C8.32843 10 9 10.6716 9 11.5H10C10 10.1193 8.88071 9 7.5 9V10ZM7.5 9C6.11929 9 5 10.1193 5 11.5H6C6 10.6716 6.67157 10 7.5 10V9ZM13.5 15C14.3284 15 15 14.3284 15 13.5H14C14 13.7761 13.7761 14 13.5 14V15ZM1.5 14C1.22386 14 1 13.7761 1 13.5H0C0 14.3284 0.671573 15 1.5 15V14Z"
-        fill="#000000"
-      />
-    </svg>
+  <svg
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <title>{"home"}</title>
+    <path
+      d="M7.5 0.5L7.8254 0.120372C7.63815 -0.0401239 7.36185 -0.0401239 7.1746 0.120372L7.5 0.5ZM0.5 6.5L0.174604 6.12037L0 6.27003V6.5H0.5ZM5.5 14.5V15C5.77614 15 6 14.7761 6 14.5H5.5ZM9.5 14.5H9C9 14.7761 9.22386 15 9.5 15V14.5ZM14.5 6.5H15V6.27003L14.8254 6.12037L14.5 6.5ZM1.5 15H5.5V14H1.5V15ZM14.8254 6.12037L7.8254 0.120372L7.1746 0.879628L14.1746 6.87963L14.8254 6.12037ZM7.1746 0.120372L0.174604 6.12037L0.825396 6.87963L7.8254 0.879628L7.1746 0.120372ZM6 14.5V11.5H5V14.5H6ZM9 11.5V14.5H10V11.5H9ZM9.5 15H13.5V14H9.5V15ZM15 13.5V6.5H14V13.5H15ZM0 6.5V13.5H1V6.5H0ZM7.5 10C8.32843 10 9 10.6716 9 11.5H10C10 10.1193 8.88071 9 7.5 9V10ZM7.5 9C6.11929 9 5 10.1193 5 11.5H6C6 10.6716 6.67157 10 7.5 10V9ZM13.5 15C14.3284 15 15 14.3284 15 13.5H14C14 13.7761 13.7761 14 13.5 14V15ZM1.5 14C1.22386 14 1 13.7761 1 13.5H0C0 14.3284 0.671573 15 1.5 15V14Z"
+      fill="#000000"
+    />
+  </svg>
 );
 
 const HomeBtnOn = (props) => (
-  <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    viewBox="0 0 15 15" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <title>{"home"}</title>
     <path d="M7.8254 0.120372C7.63815 -0.0401239 7.36185 -0.0401239 7.1746 0.120372L0 6.27003V13.5C0 14.3284 0.671573 15 1.5 15H5.5C5.77614 15 6 14.7761 6 14.5V11.5C6 10.6716 6.67157 10 7.5 10C8.32843 10 9 10.6716 9 11.5V14.5C9 14.7761 9.22386 15 9.5 15H13.5C14.3284 15 15 14.3284 15 13.5V6.27003L7.8254 0.120372Z" fill="#000000"/>
   </svg>
 );
 
 const AddBtn = (props) => (
-    <svg
+  <svg
     id="Capa_1"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -308,8 +293,6 @@ const AddBtn = (props) => (
 const AddBtnOn = (props) => (
   <svg
     fill="#000000"
-    // width="800px"
-    // height="800px"
     viewBox="0 0 1000 1000"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
@@ -320,10 +303,8 @@ const AddBtnOn = (props) => (
 );
 
 const LikeBtn = (props) => (
-    <svg
+  <svg
     fill="#000000"
-    // width="800px"
-    // height="800px"
     viewBox="0 0 36 36"
     preserveAspectRatio="xMidYMid meet"
     xmlns="http://www.w3.org/2000/svg"
@@ -341,8 +322,6 @@ const LikeBtn = (props) => (
 
 const LikeBtnOn = (props) => (
   <svg
-    // width="800px"
-    // height="800px"
     viewBox="0 0 15 15"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -357,9 +336,7 @@ const LikeBtnOn = (props) => (
 );
 
 const ProfileBtn = (props) => (
-    <svg
-    // width="800px"
-    // height="800px"
+  <svg
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -416,8 +393,6 @@ const ProfileBtnOn = (props) => (
 const LogoutBtn = (props) => (
     <svg
     fill="#000000"
-    // width="800px"
-    // height="800px"
     viewBox="0 0 36 36"
     preserveAspectRatio="xMidYMid meet"
     xmlns="http://www.w3.org/2000/svg"
